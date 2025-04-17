3cb705c73bf6ce26d933cfce2b177c0182a77887
Umar Murtaza <144917863+umar-dreamai@users.noreply.github.com>
2025-04-17 21:50:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,6 @@ def hello_world():
     return jsonify({"message": "Hello, Heroku!"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', debug=True)
+#    app.run(host='0.0.0.0', port=8000, debug=True)
+    
